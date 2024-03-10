0155ff245495d85bd5fe8911a8f4c530e514beac
SalunkeYash <yashsalunke555@gmail.com>
2024-03-10 14:45:27 +0530

    sms

--- app/src/main/java/com/example/aahaarapp/Issue.java
+++ app/src/main/java/com/example/aahaarapp/Issue.java
@@ -8,51 +8,52 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
-
 public class Issue extends AppCompatActivity {
-    EditText name,mobile,email,subject,issue;
-    String str_name,str_email,str_subject,str_issue,str_mobile;
+    EditText name, mobile, email, subject, issue;
+    String str_name, str_email, str_subject, str_issue, str_mobile, mobile_str;
     Button sbt;
     public static final String ACCOUNT_SID = "AC4c83b856d6e2158365a43f0d0e0f85dc";
     public static final String AUTH_TOKEN = "8133253477f3a026dd9b03729eb7b631";
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.issue_form);
 
-        name=findViewById(R.id.nameI);
-        mobile=findViewById(R.id.numberI);
-        email=findViewById(R.id.emailI);
-        subject=findViewById(R.id.subjectI);
-        issue=findViewById(R.id.issueI);
-        sbt=findViewById(R.id.submitI);
+        name = findViewById(R.id.nameI);
+        mobile = findViewById(R.id.numberI);
+        email = findViewById(R.id.emailI);
+        subject = findViewById(R.id.subjectI);
+        issue = findViewById(R.id.issueI);
+        sbt = findViewById(R.id.submitI);
         MyDBHelper helper = new MyDBHelper(this);
         sbt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), Signup.class));
-                str_name=name.getText().toString();
-                str_mobile=mobile.getText().toString();
-                str_email=email.getText().toString();
-                str_subject=subject.getText().toString();
-                str_issue=issue.getText().toString();
+                str_name = name.getText().toString();
+                str_mobile = mobile.getText().toString();
+                str_email = email.getText().toString();
+                str_subject = subject.getText().toString();
+                str_issue = issue.getText().toString();
 
-                helper.addNewCourse(str_name,str_mobile,str_email,str_subject,str_issue);
+                helper.addNewCourse(str_name, str_mobile, str_email, str_subject, str_issue);
 
                 // after adding the data we are displaying a toast message.
                 Toast.makeText(Issue.this, "Data Inserted", Toast.LENGTH_SHORT).show();
                 sendSms();
-
-
-                }
+            }
         });
-
     }
+
     public void sendSms() {
+        mobile = findViewById(R.id.numberI);
+        mobile_str = mobile.getText().toString();
         SmsManager smsManager = SmsManager.getDefault();
-        smsManager.sendTextMessage("+919423929864", null, "Thank you for your feedback. We will get back to you soon.", null, null);
+        smsManager.sendTextMessage(mobile_str, null, "Thank you for your feedback. We will get back to you soon.", null, null);
+        Toast.makeText(Issue.this, "SMS Send Successfully", Toast.LENGTH_SHORT).show();
     }
-
 }
--- app/src/main/java/com/example/aahaarapp/MainActivity.java
+++ app/src/main/java/com/example/aahaarapp/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.aahaarapp;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.service.carrier.CarrierService;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,5 +84,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), AdolescentGirlsAdd.class));
             }
         });
-    }
-}
+
+
+
+
+}}
